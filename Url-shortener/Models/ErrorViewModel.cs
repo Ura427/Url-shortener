14500d1b7e00bea012c5034334f415c77eb6dc4a
Yura <urakost427@gmail.com>
2023-04-16 19:57:09 +0300

    Add project files.

---
+++ Url-shortener/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Url_shortener.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
